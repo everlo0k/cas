3e376dd0d0275cf2ea796adc85ccf8f734501c5a
Hal Deadman <hal.deadman@gmail.com>
2019-08-26 19:39:38 -0400

    fix shell so it starts up

--- support/cas-server-support-shell/src/main/java/org/apereo/cas/config/CasCommandLineShellConfiguration.java
+++ support/cas-server-support-shell/src/main/java/org/apereo/cas/config/CasCommandLineShellConfiguration.java
@@ -2,12 +2,16 @@ package org.apereo.cas.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 
+import lombok.val;
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.shell.jline.PromptProvider;
+import org.springframework.webflow.config.FlowBuilderServicesBuilder;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
 /**
  * This is {@link CasCommandLineShellConfiguration}.
@@ -22,5 +26,16 @@ public class CasCommandLineShellConfiguration {
     @Bean
     public PromptProvider shellPromptProvider() {
         return () -> new AttributedString("cas>", AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+    }
+
+    /**
+     * Mock bean to satisfy autowire without adding webflow module.
+     * @return default FlowBuilderServices
+     */
+    @Lazy(false)
+    @Bean
+    public FlowBuilderServices builder() {
+        val builder = new FlowBuilderServicesBuilder();
+        return builder.build();
     }
 }
